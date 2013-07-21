78e9ad21889eaa7eae35c30e06dec74926dfea24
Benjamin Smith <benjamin.lee.smith@gmail.com>
2013-07-21 12:11:46 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SupLocator::Application.config.secret_key_base = '80fa01d9b6131e0d58889de961cfb8bae0678277effcbcb170f8aacd027084df954064d4f337c6e6466b7250633af08c104e1fd5882086a874bfedb0107d19ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SupLocator::Application.config.session_store :cookie_store, key: '_sup_locator_session'
