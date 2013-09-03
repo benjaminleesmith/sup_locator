71aba24e498f94cc88efe2b3376bcfb01d35984c
Benjamin Smith <benjamin.lee.smith@gmail.com>
2013-09-02 21:53:59 -0600

    moved pluralization of waves into engine

--- engines/web/user_location_management/config/initializers/inflections.rb
+++ engines/web/user_location_management/config/initializers/inflections.rb
@@ -8,6 +8,7 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
+  inflect.irregular 'wave', 'waves'
 end
 
 # These inflection rules are supported but not enabled by default:
