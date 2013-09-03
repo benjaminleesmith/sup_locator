38116f4997490320355f768d4f49045eab9f8932
Benjamin Smith <benjamin.lee.smith@gmail.com>
2013-09-02 20:49:00 -0600

    expose object creation methods for Users engine

--- engines/domain/users/spec/support/object_creation_methods.rb
+++ engines/domain/users/spec/support/object_creation_methods.rb
@@ -1,11 +1,13 @@
-module ObjectCreationMethods
-  def create_user(params)
-    defaults = {
-        email: "test@example.com",
-        password: "password",
-        password_confirmation: "password"
-    }
+module Users
+  module ObjectCreationMethods
+    def create_user(params)
+      defaults = {
+          email: "test@example.com",
+          password: "password",
+          password_confirmation: "password"
+      }
 
-    ::Users::User.create!(defaults.merge(params))
+      ::Users::User.create!(defaults.merge(params))
+    end
   end
 end
