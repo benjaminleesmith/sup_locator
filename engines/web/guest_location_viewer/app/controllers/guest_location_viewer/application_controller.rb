2f4829ac3b648767b98455b5fe615d342b2f77c9
Benjamin Smith <benjamin.lee.smith@gmail.com>
2013-09-08 13:18:21 -0600

    dried up current_user method

--- engines/web/guest_location_viewer/app/controllers/guest_location_viewer/application_controller.rb
+++ engines/web/guest_location_viewer/app/controllers/guest_location_viewer/application_controller.rb
@@ -1,11 +1,7 @@
 module GuestLocationViewer
-  class ApplicationController < ActionController::Base
+  class ApplicationController < SharedUi::ApplicationController
     layout "shared_ui/application"
 
     before_filter :current_user
-
-    def current_user
-      @current_user ||= Users::User.find(session[:user_id]) if session[:user_id]
-    end
   end
 end
--- engines/web/user_location_management/app/controllers/user_location_management/application_controller.rb
+++ engines/web/user_location_management/app/controllers/user_location_management/application_controller.rb
@@ -1,14 +1,10 @@
 module UserLocationManagement
-  class ApplicationController < ActionController::Base
+  class ApplicationController < SharedUi::ApplicationController
     before_filter :require_authenicated
 
     def require_authenicated
       flash[:notice] = "Please login to access that page."
       redirect_to "/" unless current_user
-    end
-
-    def current_user
-      @current_user ||= Users::User.find(session[:user_id]) if session[:user_id]
     end
   end
 end
